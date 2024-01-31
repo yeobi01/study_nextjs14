a4d5fd8c750d4cef09dbfa2848c762bd2c164cba
yeobi01 <yeobi_01@naver.com>
2024-01-31 16:46:23 +0900

    섹션 3: 클라이언트 컴포넌트에서 Server Actions 사용하기

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,48 +1,32 @@
+"use client";
+
 import style from "./signup.module.css";
-import { redirect } from "next/navigation";
+import onSubmit from "@/app/(beforeLogin)/_lib/signup";
 import BackButton from "./BackButton";
+import { useFormState, useFormStatus } from "react-dom";
+
+function showMessage(message: string | null) {
+  if (message === "no_id") {
+    return "아이디를 입력해주세요";
+  }
+  if (message === "no_name") {
+    return "닉네임을 입력해주세요";
+  }
+  if (message === "no_password") {
+    return "비밀번호를 입력해주세요";
+  }
+  if (message === "no_image") {
+    return "이미지를 업로드해주세요";
+  }
+  if (message === "user_exists") {
+    return "이미 사용 중인 아이디입니다";
+  }
+  return "";
+}
 
 export default function SignupModal() {
-  const submit = async (formData: FormData) => {
-    "use server";
-    if (!formData.get("id")) {
-      return { message: "no_id" };
-    }
-    if (!formData.get("name")) {
-      return { message: "no_name" };
-    }
-    if (!formData.get("password")) {
-      return { message: "no_password" };
-    }
-    if (!formData.get("image")) {
-      return { message: "no_image" };
-    }
-
-    let shouldRedirect = false;
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/users`,
-        {
-          method: "post",
-          body: formData,
-          credentials: "include",
-        }
-      );
-      console.log(response.status);
-      if (response.status == 403) {
-        return { message: "user_exists" };
-      }
-      console.log(await response.json());
-      shouldRedirect = true;
-    } catch (e) {
-      console.error(e);
-      return;
-    }
-
-    if (shouldRedirect) {
-      redirect("/home");
-    }
-  };
+  const [state, formAction] = useFormState(onSubmit, { message: null });
+  const { pending } = useFormStatus();
 
   return (
     <>
@@ -52,7 +36,7 @@ export default function SignupModal() {
             <BackButton />
             <div>계정을 생성하세요.</div>
           </div>
-          <form action={submit}>
+          <form action={formAction}>
             <div className={style.modalBody}>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="id">
@@ -108,9 +92,14 @@ export default function SignupModal() {
               </div>
             </div>
             <div className={style.modalFooter}>
-              <button type="submit" className={style.actionButton}>
+              <button
+                type="submit"
+                className={style.actionButton}
+                disabled={pending}
+              >
                 가입하기
               </button>
+              <div className={style.error}>{showMessage(state?.message)}</div>
             </div>
           </form>
         </div>
