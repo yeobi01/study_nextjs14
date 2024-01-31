2685f71c5d827e3df033bc51b50344a2d316a94d
yeobi01 <yeobi_01@naver.com>
2024-01-31 17:13:06 +0900

    섹션 3: next-auth로 로그인하기

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,33 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 export const {
   handlers: { GET, POST },
-  auth: {},
+  auth,
   signIn,
-} = NextAuth({});
+} = NextAuth({
+  pages: {
+    signIn: "/i/flow/login",
+    newUser: "/i/flow/signup",
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials): Promise<any> {
+        const authResponse = await fetch(`${process.env.AUTH_URL}/api/login`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            id: credentials.id,
+            password: credentials.password,
+          }),
+        });
+
+        if (!authResponse.ok) {
+          return null;
+        }
+
+        const user = await authResponse.json();
+
+        return user;
+      },
+    }),
+  ],
+});
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { auth as middleware } from "./auth";
 
 export const config = {
-  matcher: ["/compose/tweet", "/home", "/explore", "messages", "search"],
+  matcher: ["/compose/tweet", "/home", "/explore", "/messages", "/search"],
 };
